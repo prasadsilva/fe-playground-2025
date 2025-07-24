ebd1c7815a1d473da9b725ac1413821c30b81ef6
Prasad Silva <prasad.silva@gmail.com>
2025-07-23 22:29:42 -0400

    Updates

--- assets/about-BkV5mQtm.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Lk_d1Fwx.js";const r=function(){return o.jsx("div",{children:"Hello from About!"})};export{r as component};
---
+++ assets/about-DwbtW0lY.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-D8le1nlp.js";const r=function(){return o.jsx("div",{children:"Hello from About!"})};export{r as component};
